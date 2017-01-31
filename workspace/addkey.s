fbf43a9b2216b85926321ff3dfa216442708ddca
Oliver Dahl <olida686@student.liu.se>
2017-01-31 09:43:51 +0100

    more tutor tests
    todo:printstring ('GDOM BTnr...'), de/activatealarm (trap
    14),getkey (addr trap),clearinput (addr trap),addkey (addr trap)
    checkcode is working

--- workspace/addkey.s
+++ workspace/addkey.s
@@ -1,5 +1,27 @@
 setup:
-	;; ...
+	move.l #$7000,a7
+	jsr clearinput
+	move.b #1,d4
+	jsr addkey
+	move.b #2,d4
+	jsr addkey
+	move.b #3,d4
+	jsr addkey
+	move.b #4,d4
+	jsr addkey
+	move.b #255,d7
+	trap #14
+	
+clearinput:
+	;; Inargument: Inga
+	;; Utargument: Inga
+	;; Funktion: Satter innehallet pa $4000-$4003 till #$FF
+	        move.l #$ffffffff,$4000
+	        move.l #$ffffffff,$4001
+	        move.l #$ffffffff,$4002
+	        move.l #$ffffffff,$4003
+	        rts
+	
 addkey:
 	;; Inargument: Vald tangent i d4
 	;; Utargument: Inga
@@ -17,4 +39,29 @@ addkey:
 ;;; od 29/1: fick bustrap error med lsr .l $4003 sa har far vi nog anvanda d-registret
 ;;; od 29/1: kanske behover fler hopp efter cmp.b for att det ska funka
 
+activatealarm:
+;;; ; ;   samma har som de-.s las forelasnsanteckningar fran fo 04
+	                move.l $10080,$10082
+	                or #$01,$10082 ;ta\226\222nd lampan
+	                move.l $10082,$10080
+	                jsr flashdiod
+	                rts
+
+flashdiod:
+	;;  Makes the diod flash in 1 Hz.
+	        move.l d5,-(a7)	; save d5 (needed???)
+	        move.l $10080,$10082
+	        eor #$01,10082	; toggle LED
+	        move.l $10082,$10080
+	        move.w #0,d0	; counter for loop (.w 16 bit)
+	        jsr wait1
+	        rts
+
+wait1:	  add #1,d0
+	;; avbrott istallet for jsr checkkeypress...
+
+	        cmp.l #2000,d0	; delay 1 second
+	        bne wait1
+	        move.b (a7)+,d5	; reset d5 (needed???)
+	        rts
 	
--- workspace/checkcode.s
+++ workspace/checkcode.s
@@ -1,17 +1,18 @@
-;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;  Inargument: Inga
-;;  Utargument: Returnerar 1 i d4 om koden var korrekt, annars 0 i d4
-;;  här kan vi antingen ändra värden internt i checkcode eller
-;;  hoppa till ytterligare subrutiner och ändra
-checkcode
-;;  F¨orberedelseuppgift: Skriv denna subrutin
-	move.l $4000,d2 ;vi kollar input
+setup:
+	move.l #$7000,a7
+	jsr checkcode
+	move.b #255,d7
+	trap #14
+	
+checkcode:	
+	move.l $4000,d2;input hexa-tangentbord
 	move.l $4010,d3	;vi lägger fram korrekt kod
-	cmp.l £d2,d3	;vi jämföinput med korrekt kod
+	cmp.l d2,d3	;vi jämföinput med korrekt kod
 	beq correct	;om rkorrekt sätts d4 till 1
 	move.b #0,d4	;annars 0 i d4
 	rts
 correct:
 	move.b #1,d4	;checkcode hoppar hit om d2 och d3 är samma
 	rts
-;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	
