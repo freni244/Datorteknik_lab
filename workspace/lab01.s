e34019e9a478809e9c84d9c6aeb8bef8ac92059d
Oliver Dahl <olida686@student.liu.se>
2017-02-09 18:29:47 +0100

    fixed some errors together in lab and implemented...
    flashdiod, changecode and timer.

--- workspace/lab01.s
+++ workspace/lab01.s
@@ -31,16 +31,17 @@ enable:
 testcode:
 	jsr checkcode
 	cmp.b #1,d4
-	bra disable		; om ratt kod (d4=1) disable
+	beq disable		; om ratt kod (d4=1) disable
 				; annars felmedelande och enable
 wrongcode:
 	jsr printmsg
 	bra enable
 disable:
 	jsr deactivatealarm
+	jsr edit		;har kommer edit for koden for forsta gangen
 	jsr clearinput
 	;jsr setupselfactivate
-	;jsr selfactivate	; aktiverar larm efter 5 sek om ej knapp (utom A) trycks
+	;jsr selfactivate aktiverar larm efter 5 sek om ej knapp (utom A) trycks
 disable_func:	
 	jsr getkey
 	cmp.b #$a,d4
@@ -50,13 +51,18 @@ disable_func:
 activatealarm:
 ;samma har som de-.s las forelasnsanteckningar fran fo 04
 ;move.l $10080,$10082
-;or #$01,$10082  ;taÂ’nd lampan
+;or #$01,$10082  ;ta’nd lampan
 ;move.l $10082,$10080
 	move.b #$01,$10080
         rts
+edit:
+	cmp.b #$c,d4		;uppgiften fragar efter input c, d4 har input
+	beq changecode		;om sant hoppar vi till changecode (byter 4010-)
+	rts
+	
 printmsg:
 	move.l #$4020,a4
-	move.b #14,d5		;langd av'FELAKTIG KOD!'
+	move.b #15,d5		;langd av'FELAKTIG KOD!'
 	jsr printstring
 	rts
 printchar:			;utskrift i terminal
@@ -86,7 +92,7 @@ printstring:
 ;vet vi att alla tecken blir utskrivna
         move.b (a4)+,d4 	;skickar vidare M(a4) till d4
         jsr printchar   	;subrutinsanrop
-        sub.b #1,d5     	;minskar langden pa strÂ–Â’angen med 1
+        sub.b #1,d5     	;minskar langden pa str–’angen med 1
         beq skip1        	;om d5=0 hoppar vi tillbaka till setup
         bra printstring 	;annars printstring
 
@@ -107,7 +113,7 @@ getkey:
 	move.b #$00,d4
         move.b $10082,d5 ;input piab
         move.b $4005,d6  ;tidigare input
-
+	
         move.b d5,$4005 ;spara ny input
         move.b d6,$4004 ;spara gammal input
 
@@ -117,14 +123,14 @@ getkey:
         lsr.b  #4,d6   	;strobe till  bit 1
 
         cmp.b  #$0,d6 	
-        bne strobe_0	;om strobe==1  ;; skip2? dvs rts om strobe d6 var 1
+        bne skip2	;om strobe==1  ;; skip2? dvs rts om strobe d6 var 1
 strobe_0:
         cmp.b  #$1,d5 	;kollar om strobe
         bne skip2	;nar strobe = 0 => skip 2
-        move.b $4005,d4 ; Fetch input
-        and.b  #$0f,d4  ; Zero out the four MSB bits
+        move.b $4005,d4 ;input
+        and.b  #$0f,d4  
         jsr addkey
-        rts     	; Return input to d4
+        rts     	
 skip2:
         move.b #$00,d4
         rts
@@ -138,10 +144,14 @@ addkey:
         cmp.b #9,d4     ;kollar om hexkey ar 'A'->aktivaera alarm
         bgt skip3
         move.l $4000,d3
-        lsl.l #8,d3     ;flyttar 4001-4003 till 4000-4002
-        move.l d3,$4000
+	move.b $4000,d7
+	move.l d7,$4006
+	lsl.l #8,d7
+	lsl.l #8,d3     ;flyttar 4001-4003 till 4000-4002
+	move.l d3,$4000
         move.b d4,$4003 ;flyttar tillbaka d3 till 4 senaste hexa-tecknen
-        rts             ;ska det inte vara 4001-4003 som flyttas till 4000-4002
+	
+	rts             ;ska det inte vara 4001-4003 som flyttas till 4000-4002
 ;;; ; blir det inte lsr.l #8 4003 ($a $b $c $d) -> ($0 $a $b $c)
 ;;; ; sedan move.l d4,4003 ($d4 $a $b $c)
 ;;; ; Obs: inte alls saker
@@ -154,10 +164,8 @@ clearinput:
 ;Inargument: Inga
 ;Utargument: Inga
 ;Funktion: Satter innehallet pa $4000-$4003 till #$FF
-	move.b #$ff,$4000
-        move.b #$ff,$4001
-        move.b #$ff,$4002
-	move.b #$ff,$4003
+	move.b #$ffffffff,$4000
+	move.b #$ffffffff,$4006
 	rts
 
 checkcode:
@@ -188,6 +196,7 @@ printerror:
         move.b #'O',$402A
         move.b #'D',$402B
         move.b #'!',$402C
+	move.b #10,$402D	;newline
 	rts
 code:	
 	move.b #$00,$4010	;kombination for avlarmning
@@ -197,6 +206,9 @@ code:
 	rts
 
 update_diod:
+	cmp.b #1,$4300
+	beq activatealarm
+	bne deactivatealarm
 	move.l $4300,$10080	; lagger till ev. ny info pa 10080
 	rts
 
@@ -213,19 +225,18 @@ flashdiod:
 	;cmp.b #1,d5
 	;beq getkey		;om strobe=1 hoppa till getkey
 	; alt om strobe finns sparad
-	
 	move.l $4400,d1 	;rakanre till d1
 	add.l #1,d1		;+1
 	move.l d1,$4400		;spara
-
+	
 	move.l $4400,d2		;fordrojning till d2
-	cpm.l d1,d2		;raknat klart?
+	cmp.l d1,d2		;raknat klart?
 	beq togglediod		;toggla om sant, annars hoppa tillbaka till enable
 	rts
 
 togglediod:
 	move.b $4300,d3		;tillstand till d3
-	xor.b #01,d3		;toggla tillstand pa bit 1
+	eor.b #01,d3		;toggla tillstand pa bit 1
 	move.b d3,$4300		;spara tillstand
 	move.l #0,$4400		;nollstall raknare
 	rts
@@ -245,33 +256,26 @@ wait:
 	jsr getkey		;;beroende pa om getkey vantar pa input
 				;;annars kan ha d4 till nagot stort och ta blt f, sa 
 				;;eller stroben
-	cmp.b #f,d4
+	cmp.b #$f,d4
 	blt checkA      	;hex-key d4 < F (dvs ingen knapptryckning) branch
 
 	add #1,d0
-	cpm $4500,d0
-	ben wait		;tid < 5 sek => wait
+	cmp.l $4500,d0
+	bne wait		;tid < 5 sek => wait
 	jsr enable		;5 har passerat
 	rts
 
 checkA:
-	cpm #$a,d4
+	cmp.b #$a,d4
 	beq enable		;om d4 = A => enable alarm
 	bra disable_func	;annars hoppa till disable_funk
 	rts
 
-
-
-selfactivate:
-	move.l $4600,d0		;raknare till d0
-	add #1,d0		;+1
-	move.l d0,$4600		;spara
-	cpm $4500,d0		;jamfor raknare med 5s fordrojning
-	beq enable		;enable om 5 sekunder passerat
-
-	cmp.b #9,d4
-	bgt
-	rts
-
-
-
+changecode:
+	move.l $4000,a1		;sparar 32bit input (4000-4003) i a1
+	move.l $4006,a2		;sparar 32bit tidigare input (4006-4006) i a2
+	cmp.l a1,a2		;har vi skrivit in samma 4siffriga kod 2 ganger?
+	bne skip4		;nej->skippa kodbytet
+	move.l a1,$4010		;ja->byt kod
+	
+skip4:	rts
