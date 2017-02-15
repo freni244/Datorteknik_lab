5ee730a34d04d40070a39b2bd9ec62925d8ab095
Fredrik Nilsson <freni244@student.liu.se>
2017-02-15 21:56:41 +0100

    fixa en sak jag missade i förra comitten + fundering
    Fundering: står som komentar. Byta, eller läsa PIAA/B för att
    nollställa flaggor i CRA/B?

--- workspace/Lab 2/lab2.s
+++ workspace/Lab 2/lab2.s
@@ -5,7 +5,7 @@ setup:
 	jsr PINIT		; given PIA-setup
 	move.l AVBV,$1100	; lagger avbrottsrutiner pa 1100 resp 1200
 	move.l AVBH $1200
-	move.l $1100,$068	; flyttar adress 1100 till avbr.vektror med avbr.niva-2 ;;???
+	move.l $1100,$068	; flyttar adress 1100 till avbr.vektror med avbr.niva-2
 	move.l $1200,$074	; Ñ..- 1200 -..- avbrottsniva-5
 	and.w #F8FF,sr		; andrar avbrottsnivan i processorn till 0
 
@@ -20,7 +20,11 @@ AVBV:
 	rte
 
 AVBH:
-	cmp.b #0,$10080		; laser fran PIAB Ñ> nolstaller flaggor i CRB
+	cmp.b #0,$10082		; laser fran PIAB Ñ> nolstaller flaggor i CRB
 	jsr SKAVH		; $20A6
 	rte
 
+; Kanske detta, beroende pa vad som menas med laser fran PIAA/B under 4.7
+; menar dem att man byter till PIAA/B eller laser fran den?
+; or.b #4,10080
+; or.b #4,10082
