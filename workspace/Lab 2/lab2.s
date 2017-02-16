4ec0f7157431cf1ed29e3d1b56037263b0bdf273
Fredrik Nilsson <freni244@student.liu.se>
2017-02-16 11:42:24 +0100

    Uppgift 1 och 2 klara (ej testade)

--- workspace/Lab 2/lab2.s
+++ workspace/Lab 2/lab2.s
@@ -1,19 +1,16 @@
 
-; uppgift 1
+
 setup:
 	move.l #$7000,a7	; laddar stackpekaren
+	g c300			; hamtar givna subrutiner
 	jsr PINIT		; given PIA-setup
-	move.l AVBV,$1100	; lagger avbrottsrutiner pa 1100 resp 1200
-	move.l AVBH $1200
-	move.l $1100,$068	; flyttar adress 1100 till avbr.vektror med avbr.niva-2
-	move.l $1200,$074	; Ñ..- 1200 -..- avbrottsniva-5
+	move.l #AVBV,$068	; flyttar adress till AVBV som avbr.vektror med avbr.niva-2
+	move.l #AVBH,$074	; Ñ..- AVBH -..- avbrottsniva-5
 	and.w #F8FF,sr		; andrar avbrottsnivan i processorn till 0
-
-printBAKGRUNDSPROGRAM:
-
-
-; uppgift 2
-
+msg_loop:
+	jsr SKBAK
+	bra msg_loop
+	
 AVBV:
 	cmp.b #0,$10080		; laser fran PIAA Ñ> nolstaller flaggor i CRA, se 4.7
 	jsr SKAVV		; $2048
@@ -24,7 +21,3 @@ AVBH:
 	jsr SKAVH		; $20A6
 	rte
 
-; Kanske detta, beroende pa vad som menas med laser fran PIAA/B under 4.7
-; menar dem att man byter till PIAA/B eller laser fran den?
-; or.b #4,10080
-; or.b #4,10082
