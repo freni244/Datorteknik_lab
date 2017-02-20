7ab536d40d31280c9f4a584c9a6e5c2cd8d7f495
Fredrik Nilsson <freni244@student.liu.se>
2017-02-20 15:59:33 +0100

    lade till $-tecken och kommenterade bort g C300.

--- workspace/Lab 2/lab2.s
+++ workspace/Lab 2/lab2.s
@@ -1,16 +1,17 @@
 
+;g c300			; hamtar givna subrutiner
 
 setup:
 	move.l #$7000,a7	; laddar stackpekaren
-	g c300			; hamtar givna subrutiner
 	jsr PINIT		; given PIA-setup
 	move.l #AVBV,$068	; flyttar adress till AVBV som avbr.vektror med avbr.niva-2
 	move.l #AVBH,$074	; Ñ..- AVBH -..- avbrottsniva-5
-	and.w #F8FF,sr		; andrar avbrottsnivan i processorn till 0
+	and.w #$F8FF,sr		; andrar avbrottsnivan i processorn till 0
 msg_loop:
-	and.w #FEFF,sr		; andrar avbrottsnivan i processorn till &6 = %110 (%1111 1110 1111 1111)
+	or.w #$0600,sr		; andrar avbrottsnivan i processorn till 6
 	jsr SKBAK		; nu kan bara rod knapp avbryta under utskrift
-	and.w #F8FF,sr		; avbrottsniva 0
+	and.w #$F8FF,sr		; avbrottsniva 0
+	
 	move.b #&1000,d0	; flyttar 1000 till d0
 	jsr DELAY		; 1000 ms = 1 s fordrojning
 	bra msg_loop
